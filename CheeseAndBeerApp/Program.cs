72840b33b2355ecc700ee68ae290c12e87864e63
Evie Skinner <evie.skinner@gwle.onmicrosoft.com>
2019-02-20 18:00:40 +0000

    Following a tutorial on how to consume a RESTful service. Will communicate with teh Punk API through HttpClient class

--- CheeseAndBeerApp/Program.cs
+++ CheeseAndBeerApp/Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
 
 namespace CheeseAndBeerApp
 {
     class Program
     {
+        private static HttpClient client = new HttpClient();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to the cheese and beer zone! Please enter your favourite cheese to begin.");
+            ProcessBeers().Wait();
+        }
+
+        private static async Task ProcessBeers()
+        {
+
         }
     }
 }
